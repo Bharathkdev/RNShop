dc3f58fd3217aafb07a399f91c511061c012a110
Bharathkdev <bharathaskkarur46@gmail.com>
2023-10-28 00:15:49 +0530

    refactor(tabbar): extracted the styles object from tabBar and moved it into a seperate component
    Extracted the styles object from tabBar component and moved it into a seperate component for better
    code readability

---
+++ src/navigation/TabBar.style.tsx
@@ -0,0 +1,43 @@
+import {StyleSheet, ViewStyle, TextStyle} from 'react-native';
+import {moderateScale} from 'react-native-size-matters';
+import {colors} from '../common/theme/colors';
+import {commonTheme} from '../common/theme';
+
+interface stylePropTypes {
+  tabBarContainer: ViewStyle;
+  tabItem: ViewStyle;
+  tabItemContainer: ViewStyle;
+  tabLabel: TextStyle;
+  animatedTab: ViewStyle;
+}
+
+export const styles = StyleSheet.create<stylePropTypes>({
+  tabBarContainer: {
+    flexDirection: 'row',
+    backgroundColor: colors.dark,
+    padding: moderateScale(5),
+    height: '10%',
+    width: '100%',
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabItemContainer: {
+    alignItems: 'center',
+  },
+  tabLabel: {
+    fontSize: commonTheme.fontSizes.m,
+    paddingBottom: moderateScale(10),
+  },
+  animatedTab: {
+    position: 'absolute',
+    bottom: moderateScale(5),
+    height: '100%',
+    width: '50%',
+    padding: moderateScale(5),
+    backgroundColor: colors.primary,
+    borderRadius: moderateScale(10),
+  },
+});
--- src/navigation/TabBar.tsx
+++ src/navigation/TabBar.tsx
@@ -1,57 +1,10 @@
 import React, {useEffect, useRef} from 'react';
-import {
-  View,
-  Animated,
-  TouchableOpacity,
-  StyleSheet,
-  ViewStyle,
-  TextStyle,
-} from 'react-native';
+import {View, Animated, TouchableOpacity} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import {moderateScale} from 'react-native-size-matters';
 import {colors} from '../common/theme/colors';
 import {Label} from '../common/components/Label';
-import {commonTheme} from '../common/theme';
 import {TabBarProps} from './index';
-
-interface stylePropTypes {
-  tabBarContainer: ViewStyle;
-  tabItem: ViewStyle;
-  tabItemContainer: ViewStyle;
-  tabLabel: TextStyle;
-  animatedTab: ViewStyle;
-}
-
-const styles = StyleSheet.create<stylePropTypes>({
-  tabBarContainer: {
-    flexDirection: 'row',
-    backgroundColor: colors.dark,
-    padding: moderateScale(5),
-    height: '10%',
-    width: '100%',
-  },
-  tabItem: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tabItemContainer: {
-    alignItems: 'center',
-  },
-  tabLabel: {
-    fontSize: commonTheme.fontSizes.m,
-    paddingBottom: moderateScale(10),
-  },
-  animatedTab: {
-    position: 'absolute',
-    bottom: moderateScale(5),
-    height: '100%',
-    width: '50%',
-    padding: moderateScale(5),
-    backgroundColor: colors.primary,
-    borderRadius: moderateScale(10),
-  },
-});
+import {styles} from './TabBar.style';
 
 export const TabBar: React.FC<TabBarProps> = ({
   state,
