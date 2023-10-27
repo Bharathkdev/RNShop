2825b157cab2cf8fb7529b1db5485027f2b35944
Bharathkdev <bharathaskkarur46@gmail.com>
2023-10-28 00:11:42 +0530

    refactor(strings.ts): added common strings file which holds all the components static strings
    Added common strings file which holds all the components static strings and error handling strings

---
+++ src/common/strings.ts
@@ -0,0 +1,32 @@
+export const strings = {
+  TabBar: {
+    homeTitle: 'Home',
+    searchTitle: 'Search',
+  },
+  HomeScreen: {
+    discountBannerText: 'Delivery is 50% discount',
+    noMoreData: 'No more items!',
+  },
+  ProductDetailsScreen: {
+    price: 'Price',
+    brand: 'Brand',
+    stock: 'In Stock',
+  },
+  SearchScreen: {
+    results: 'Results',
+    noResults: 'No results found!',
+    placeHolder: 'Search products',
+    topSearches: 'Top Searches',
+    categories: 'Categories',
+    seeLess: 'See Less',
+    seeMore: 'See More',
+  },
+  ErrorHandling: {
+    productListError: 'Error fetching product list. Please try again later.',
+    searchProductError: 'Error searching products. Please try again later.',
+    categoriesListError:
+      'Error fetching categories list. Please try again later.',
+    productListByCategoryError:
+      'Error fetching products by category. Please try again later.',
+  },
+};
